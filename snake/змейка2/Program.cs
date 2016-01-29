816b71a82505baa736c5acd25a8b51f3900fb927
danaibraimova <ibraimova.dana@gmail.com>
2016-01-30 02:32:24 +0600

    snake

---
+++ snake/змейка2/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace змейка2
+{
+    interface IDrawable
+    {
+        void Draw();
+    }
+    public class Point 
+    {
+        public int x;
+        public int y;
+        public Point(int _x, int _y)
+        {
+            x =_x;
+            y = _y;
+        }
+
+    }
+
+    public class Snake : IDrawable
+    {
+        public List<Point> body; 
+        public Snake()
+        {
+            body= new List<Point>();
+            body.Add(new Point(10,10));
+          
+        }
+
+        public void Draw()
+        {
+            foreach(Point p in body)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.SetCursorPosition(p.x, p.y);
+                Console.Write("*");
+            }
+        }
+
+        public void Clear()
+        {
+            foreach (Point p in body)
+            {
+                Console.SetCursorPosition(p.x, p.y);
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.Write(' ');
+            }
+        }
+
+        public bool Move(int dx, int dy, Point food)
+        {
+            Clear();
+            Point head = body[0];
+            if(head.x+dx==food.x && head.y+dy==food.y)
+            {
+                body.Add(food);             
+                return true; //true => появляется еда
+            }
+
+            for (int i = body.Count - 1; i > 0; i--)
+            {
+                body[i].x = body[i - 1].x;
+                body[i].y = body[i - 1].y;
+            }//чтобы остальные части тела двигались за головой
+            
+            Point p = body[1];
+            while(p != body[body.Count - 1])
+            {
+                
+                if(head.x==p.x && head.y==p.y)
+                {
+                    throw new Exception();
+                }
+                p.x++;
+                p.y++;
+            }
+
+
+
+                head.x += dx;
+            head.y += dy;
+            Draw();
+            return false; //не появляется еда
+
+            
+        }
+
+
+        
+    }
+
+    public class Food : IDrawable
+    {
+        public Point pos=new Point(0,0);
+        public Food()
+        {
+            Draw();
+            
+        }
+
+        public void Draw()
+        {
+            pos.x = new Random().Next() % 60;
+            pos.y = new Random().Next() % 40;
+            Console.SetCursorPosition(pos.x, pos.y);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("^");
+        }
+    }
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(60,40);
+
+            Snake sn = new Snake();
+            sn.Draw();
+            Food f = new Food();
+            try
+            {
+                while (true)
+                {
+
+                    ConsoleKeyInfo pressedKey = Console.ReadKey();
+
+
+                    switch (pressedKey.Key)
+                    {
+
+                        case ConsoleKey.UpArrow:
+                            while (sn.Move(0, -1, f.pos)) f = new Food();
+                            break;
+
+                        case ConsoleKey.DownArrow:
+                            while (sn.Move(0, 1, f.pos)) f = new Food();
+                            break;
+
+                        case ConsoleKey.LeftArrow:
+                            while (sn.Move(-1, 0, f.pos)) f = new Food();
+                            break;
+
+                        case ConsoleKey.RightArrow:
+                            while (sn.Move(1, 0, f.pos)) f = new Food();
+                            break;
+
+                        case ConsoleKey.Escape:
+                            break;
+
+                    }
+
+
+                }
+            }
+            catch (Exception)
+            {
+                Console.Clear();
+                Console.WriteLine("You lost!");
+                Console.WriteLine();
+                Console.WriteLine("or something went wrong. game over anyway");
+                Console.ReadKey();
+            }
+      
+           // Timer t = new Timer(TimerCallback, null, 0, 1000);
+           // for (; ; ) { }
+                 
+        }
+        //private static void TimerCallback(Object o)
+        //{
+
+            
+        //}
+    }
+}
---
+++ snake/змейка2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("змейка2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("змейка2")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("c18e3ead-f791-4623-bbdb-ffddd4761dcb")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
